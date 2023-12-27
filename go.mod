db958687d7e90310491ee51d40e3dc6c0a802d05
Guil. Sperb Machado <guil@axlabs.com>
2023-12-27 14:41:29 +0100

    working code to sign data from the HSM \o/

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AxLabs/go-yubico-gsm-example
 
 go 1.21.3
 
-require github.com/miekg/pkcs11 v1.1.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/miekg/pkcs11 v1.1.1
+)
--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-import "github.com/miekg/pkcs11"
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/miekg/pkcs11"
+)
+
+const (
+	authKeyObjId       = "0002"
+	authKeyPassword    = "newpassword123"
+	asymmetricKeyLabel = "hsm-go-test-key1"
+	dataFilePath       = "./data.txt"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
 
-	p := pkcs11.New("/usr/local/Cellar/p11-kit/0.25.3/lib/pkcs11/yubihsm_pkcs11.dylib")
-	err := p.Initialize()
+	libPath := os.Getenv("PKCS11_LIB_PATH")
+	if libPath == "" {
+		panic("PKCS11_LIB_PATH is not set")
+	}
+
+	p := pkcs11.New(libPath)
+	err = p.Initialize()
 	if err != nil {
 		panic(err)
 	}
@@ -23,24 +52,237 @@ func main() {
 	}
 	defer p.CloseSession(session)
 
-	err = p.Login(session, pkcs11.CKU_USER, "0001password")
+	err = p.Login(session, pkcs11.CKU_USER, authKeyObjId+authKeyPassword)
 	if err != nil {
 		panic(err)
 	}
 	defer p.Logout(session)
 
-	template := []*pkcs11.Attribute{
-		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
-		pkcs11.NewAttribute(pkcs11.CKA_LABEL, "password"),
-	}
-	if err = p.FindObjectsInit(session, template); err != nil {
-		panic(err)
-	}
-	_, _, err = p.FindObjects(session, 1)
+	// Find all objects
+	err = p.FindObjectsInit(session, nil) // No template means "all objects"
 	if err != nil {
 		panic(err)
 	}
-	if err = p.FindObjectsFinal(session); err != nil {
+
+	// Retrieve objects
+	objects, _, err := p.FindObjects(session, 100)
+	if err != nil {
 		panic(err)
+	}
+
+	err = p.FindObjectsFinal(session)
+	if err != nil {
+		panic(err)
+	}
+
+	// List objects
+	fmt.Printf("Found %d object(s)\n", len(objects))
+	for _, obj := range objects {
+		// Convert object handle to hex
+		objIDHex := fmt.Sprintf("0x%016x", obj)
+
+		// Get label attribute of the object
+		labelAttr := []*pkcs11.Attribute{
+			pkcs11.NewAttribute(pkcs11.CKA_LABEL, nil),
+		}
+		labelAttrValue, err := p.GetAttributeValue(session, obj, labelAttr)
+		if err != nil {
+			fmt.Printf("Error getting label for object ID %s: %v\n", objIDHex, err)
+			continue
+		}
+
+		// Get class attribute of the object
+		classAttr := []*pkcs11.Attribute{
+			pkcs11.NewAttribute(pkcs11.CKA_CLASS, nil),
+		}
+		classAttrValue, err := p.GetAttributeValue(session, obj, classAttr)
+		if err != nil {
+			fmt.Printf("Error getting class for object ID %s: %v\n", objIDHex, err)
+			continue
+		}
+
+		// Read attributes
+		label := ""
+		if len(labelAttrValue[0].Value) > 0 {
+			label = string(labelAttrValue[0].Value)
+		}
+		class := ""
+		if len(classAttrValue[0].Value) > 0 {
+			class = fmt.Sprintf("0x%x", classAttrValue[0].Value)
+		}
+
+		fmt.Printf("Object ID: %s, Label: %s, Class: %s\n", objIDHex, label, class)
+	}
+
+	// Find an asymmetric key private key object
+	templatePrivKeyObj := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PRIVATE_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, asymmetricKeyLabel),
+	}
+
+	// Find private key object
+	err = p.FindObjectsInit(session, templatePrivKeyObj)
+	if err != nil {
+		panic(err)
+	}
+
+	objectsPrivKey, _, err := p.FindObjects(session, 10)
+	if err != nil {
+		panic(err)
+	}
+
+	err = p.FindObjectsFinal(session)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check if the private key is found
+	if len(objectsPrivKey) == 0 {
+		fmt.Println("Private key not found")
+		return
+	}
+	privateKeyObject := objectsPrivKey[0]
+
+	// Let's make sure that I can't read the private key value ;-)
+	// Trying to get the private key value...
+	getPrivKeyAttrTemplate := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_VALUE, nil),
+	}
+	getPrivKeyAttrValue, err := p.GetAttributeValue(session, privateKeyObject, getPrivKeyAttrTemplate)
+	if err != nil && len(getPrivKeyAttrValue) == 0 {
+		fmt.Printf("Private Key is properly protected.\n")
+	} else {
+		panic("This should never happen. Private key value was leaked!")
+	}
+
+	// Find an asymmetric key (public key) object
+	templatePubKeyObj := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_LABEL, asymmetricKeyLabel),
+	}
+	err = p.FindObjectsInit(session, templatePubKeyObj)
+	if err != nil {
+		panic(err)
+	}
+	objectsPubKey, _, err := p.FindObjects(session, 10)
+	if err != nil {
+		panic(err)
+	}
+	err = p.FindObjectsFinal(session)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check if the public key is found
+	if len(objectsPubKey) == 0 {
+		fmt.Println("Public key not found")
+		return
+	}
+	publicKeyObject := objectsPubKey[0]
+
+	// Convert object handle to hex
+	publicKeyObjectIDHex := fmt.Sprintf("0x%016x", publicKeyObject)
+	fmt.Printf("Using Public Key with Object ID: %s\n", publicKeyObjectIDHex)
+
+	// Get the public key value
+	getPubKeyAttrTemplate := []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_VALUE, nil),
+	}
+	getPubKeyAttrValue, err := p.GetAttributeValue(session, publicKeyObject, getPubKeyAttrTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	publicKeyValue := getPubKeyAttrValue[0].Value
+	fmt.Printf("Public Key in Hex: %s\n", hex.EncodeToString(publicKeyValue))
+	fmt.Printf("Public Key in Base64: %s\n", base64.StdEncoding.EncodeToString(publicKeyValue))
+
+	// Read the data file to be signed
+	data, err := os.ReadFile(dataFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Hash the data
+	hashSignature := sha256.Sum256(data)
+
+	// Request the HSM to sign the hash
+	mechanism := []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_ECDSA, nil)}
+	err = p.SignInit(session, mechanism, privateKeyObject)
+	if err != nil {
+		panic(err)
+	}
+	signature, err := p.Sign(session, hashSignature[:])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Signature in Hex: %s\n", hex.EncodeToString(signature))
+	fmt.Printf("Signature in Base64: %s\n", base64.StdEncoding.EncodeToString(signature))
+
+	// Parse the public key
+	pubKey, err := parseECPublicKeyFromBytes(publicKeyValue)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the curve name based on the public key
+	printCurveName(pubKey)
+
+	// Prepare the signature for verification (extract r and s)
+	r := big.NewInt(0).SetBytes(signature[:32])
+	s := big.NewInt(0).SetBytes(signature[32:])
+
+	// Verify the signature
+	hashVerification := sha256.Sum256(data)
+	verified := ecdsa.Verify(pubKey, hashVerification[:], r, s)
+	if !verified {
+		fmt.Println("Verification failed")
+	} else {
+		fmt.Println("Verification successful")
+	}
+
+}
+
+func parseECPublicKeyFromBytes(pubBytes []byte) (*ecdsa.PublicKey, error) {
+	pubKey, err := x509.ParsePKIXPublicKey(pubBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch pub := pubKey.(type) {
+	case *ecdsa.PublicKey:
+		return pub, nil
+	default:
+		return nil, fmt.Errorf("not an ECDSA public key")
+	}
+}
+
+func parseECPublicKeyFromBase64(pubBase64 string) (*ecdsa.PublicKey, error) {
+	pubBytes, err := base64.StdEncoding.DecodeString(pubBase64)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseECPublicKeyFromBytes(pubBytes)
+}
+
+func printCurveName(pubKey *ecdsa.PublicKey) {
+	switch pubKey.Curve {
+	case elliptic.P224():
+		fmt.Println("Curve: P-224 (secp224r1)")
+	case elliptic.P256():
+		fmt.Println("Curve: P-256 (secp256r1)")
+	case elliptic.P384():
+		fmt.Println("Curve: P-384 (secp384r1)")
+	case elliptic.P521():
+		fmt.Println("Curve: P-521 (secp521r1)")
+	default:
+		// Not sure if this works...
+		switch pubKey.Curve.Params().Name {
+		case "secp256k1":
+			fmt.Println("Curve: secp256k1")
+		default:
+			fmt.Println("Curve: Unknown")
+		}
 	}
 }
